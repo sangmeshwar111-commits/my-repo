7c025364e8c3b6ddb73de5e2a945c4c90a4e4322
sangmeshwar111-commits <sangmeshwar111@gmail.com>
2026-01-18 10:59:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using EmployeeManagementAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+app.MapControllers();
+app.Run();
